b870440d847d7d3a81753a5de314c51853965cbc
Nur Sima Akgül <akgulnursima1712@gmail.com>
2026-02-04 03:28:56 +0300

    Force cache refresh with v=0327 and lift mobile catcher to 0.15

--- script.js
+++ script.js
@@ -668,8 +668,8 @@ class Gun {
         this.pivotX = canvasWidth * xOffset;
 
         // Sabit piksel yerine ekranın yüzdesiyle konumlandır (Zemine yapışması için)
-        // Mobilde daha da yukarı kaldırıldı (0.28 -> 0.22) ve PC konumu korundu
-        this.pivotY = isMobile ? canvasHeight * 0.22 : canvasHeight * 0.68;
+        // Mobilde ÇOK DAHA yukarı kaldırıldı (0.22 -> 0.15) ve PC konumu korundu
+        this.pivotY = isMobile ? canvasHeight * 0.15 : canvasHeight * 0.68;
     }
 
     update() {
